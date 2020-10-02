b53986e86062cbe7f3119b2a8bde0f55c479e822
trushchinskii-surf <trushchinskii@surfstudio.ru>
2020-10-02 16:57:19 +0300

    Revert "no-task - update plugins from master"
    This reverts commit 91e86114

--- packages/video_player/video_player/test/video_player_test.dart
+++ packages/video_player/video_player/test/video_player_test.dart
@@ -48,7 +48,7 @@ class FakeController extends ValueNotifier<VideoPlayerValue>
   Future<void> setPlaybackSpeed(double speed) async {}
 
   @override
-  Future<void> initialize({Duration duration}) async {}
+  Future<void> initialize() async {}
 
   @override
   Future<void> pause() async {}
